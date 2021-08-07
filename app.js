7c5145104c07ed96163fc516f116d7ace68ab19e
AJ Lovelace <lovelace.a.1@gmail.com>
2021-08-07 08:14:01 -0400

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -49,52 +51,23 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
-
-// basic authentication
-// function auth(req, res, next) {
-//   console.log(req.headers);
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//     const err = new Error("You are not authenticated!");
-//     res.setHeader("WWW-Authenticate", "Basic");
-//     err.status = 401;
-//     return next(err);
-//   }
-
-//   const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-//     .toString()
-//     .split(":");
-//   const user = auth[0];
-//   const pass = auth[1];
-//   if (user === "admin" && pass === "password") {
-//     return next(); // authorized
-//   } else {
-//     const err = new Error("You are not authenticated!");
-//     res.setHeader("WWW-Authenticate", "Basic");
-//     err.status = 401;
-//     return next(err);
-//   }
-// }
 
 app.use(auth);
 
